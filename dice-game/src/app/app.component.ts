62d410fcf09974a41d0b64aa93c168600a4bb38e
Logan <loganlan2001@gmail.com>
2021-06-29 16:45:57 -0700

    Started on the start page

--- dice-game/src/app/app.component.ts
+++ dice-game/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Ma}
+
 
 @Component({
   selector: 'app-root',
--- dice-game/src/app/app.module.ts
+++ dice-game/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialsModule } from './materials/materials.module';
+import { StartPageComponent } from './start-page/start-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialsModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]

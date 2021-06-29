350255fe7ace1e868397ab57c39e2b8620de056f
Gabe Juarez <empirenerd1138@gmail.com>
2021-06-29 14:54:42 -0700

    Setting up my Branch

--- dice-game/src/app/app.module.ts
+++ dice-game/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { GameBoardPageComponent } from './game-board-page/game-board-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dice-game/src/app/game-board-page/game-board-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-board-page',
+  templateUrl: './game-board-page.component.html',
+  styleUrls: ['./game-board-page.component.css']
+})
+export class GameBoardPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
